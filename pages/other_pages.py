ebbde5ae65ca400419c556cd6fbe79b86c7c0e7c
Lavandazz <167910387+Lavandazz@users.noreply.github.com>
2025-04-16 00:19:07 +0300

    добавлена страница с основными моментами банкротства

--- pages/other_pages.py
+++ pages/other_pages.py
@@ -17,3 +17,15 @@ async def politics(request: Request):
                                                                           "title": title,
                                                                           "keywords": keywords,
                                                                           "description": description})
+
+
+@router.get("/osnovnye-momenty-procedury-bankrotstva", response_class=HTMLResponse, tags=["other pages"])
+async def main_points_procedure(request: Request):
+    """ Страница основных моментов процедуры банкротства фаз лица """
+    title = "Основные моменты процедуры банкротства физических лиц"
+    keywords = "гражданин, банкротство, суд, документы"
+    description = "Что такое «банкротство гражданина? Банкротство физического лица"
+    return jinja_env.TemplateResponse("other_pages/main_points.html", {"request": request,
+                                                                       "title": title,
+                                                                       "keywords": keywords,
+                                                                       "description": description})
